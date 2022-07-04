6154aee1a422ae7c46040f021c699a6948bd1e85
Nathan Nunes <nathanpedronunes@hotmail.com>
2022-07-04 15:02:14 -0300

    update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
+from shutil import ExecError
 from pdf import *
 
-nome = input("Nome: ")
-rg = input("RG: ")
+while True:
+    print("MENU: \n1 - Gerar Documento\n2 - Visualizar Documento\n3 - Imprimir Documento")
+    while True:
+        try:
+            opc = int(input("Digite a opção desejada: "))
+            break
+        except ValueError:
+            print("Valor inválido, tente novamente!")
 
-newPDF(nome, rg)
+    if opc == 1:
+        nome = input("Nome: ")
+        rg = input("RG: ")
+
+        newPDF(nome, rg)
+
+    elif opc == 2:
+        pass
+
+    elif opc == 3:
+        pass
+
+    else:
+        print("Opção inválida, tente novamente!")
+
+    print("")
